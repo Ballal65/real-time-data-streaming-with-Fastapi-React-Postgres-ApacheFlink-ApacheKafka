db27947d34c2e5060e0e3fa4f253e46c1e37f76b
Ballal65 <ballalpathare65@gmail.com>
2025-01-13 11:10:41 +0530

    First commit

---
+++ backend/app/database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Load environment variables
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+print(POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_SERVER, POSTGRES_PORT, POSTGRES_DB)
+
+# ✅ Use the service name directly
+POSTGRES_SERVER = "postgres"
+
+# Postgres connection URL
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+# Create the SQLAlchemy engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a sessionmaker
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ backend/app/main.py
@@ -0,0 +1,96 @@
+import sys
+import json
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import vendors
+from fastapi.requests import Request
+from confluent_kafka import Producer
+from app.database import Base, engine
+
+#If we don't import this FastAPI will not create a table
+from app.models import Vendor 
+
+# Kafka producer configuration
+producer_config = {
+    'bootstrap.servers': 'kafka:9092',
+    'client.id': 'fastapi-producer',
+    'acks': 'all',
+    'compression.type': 'gzip',
+    'linger.ms': 5,
+    'batch.size': 16384
+}
+producer = Producer(producer_config)
+
+app = FastAPI()
+
+#Binding the database with main.py
+Base.metadata.create_all(bind=engine)
+
+app.include_router(vendors.router)
+# CORS origins
+ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+    # Add other origins as needed
+]
+
+#Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Middleware to log request data
+@app.middleware("http")
+async def log_request_data(request: Request, call_next):
+    # Extract request metadata
+    client_host = request.client.host
+    method = request.method
+    url = request.url.path
+    headers = dict(request.headers)
+
+    # Log the request metadata
+    print(f"Client Host: {client_host}")
+    print(f"Method: {method}")
+    print(f"URL: {url}")
+    print(f"Headers: {headers}")
+    # Process the request and get the response
+
+    event_message = {
+        "client_host": client_host,
+        "method": method,
+        "url": url,
+        "headers": headers
+    }
+
+    # Convert the event message to a JSON string
+    event_message_json = json.dumps(event_message)
+
+    # Send the message to Kafka
+    producer.produce('ui-event-log', key=url, value=event_message_json)
+    producer.flush()
+
+    response = await call_next(request)
+    return response
+
+@app.get("/")
+async def get_message():
+    return {"Message": "Hello from FastAPI."}
+
+if __name__ == "__main__":
+    print(f"By default reload state is False.")
+    print("Available CLI argument: Reload state (True/False)")
+
+    args = sys.argv[1:]
+    reload = False
+
+    if len(args) == 1:
+        if args[0].lower() == 'true':
+            reload = True
+    print(f"Reload State: {reload}")
+
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=reload)
